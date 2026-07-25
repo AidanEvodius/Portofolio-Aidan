868c4c820f114135af204cc71ef3561ccfdda80b
AidanEvodius <evodiusaidan@gmail.com>
2026-07-25 18:32:54 +0700

    proyek aplikasi netbeans

---
+++ src/main/java/com/mycompany/aplikasiterasrumahtr/AplikasiTerasRumahTR.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aplikasiterasrumahtr;
+
+/**
+ *
+ * @author lenovo
+ */
+public class AplikasiTerasRumahTR {
+
+    public static void main(String[] args) {
+       new MenuUtama().setVisible(true);
+    }
+}
